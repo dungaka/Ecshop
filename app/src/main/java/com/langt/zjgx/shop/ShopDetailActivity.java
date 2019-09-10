6a9a2b7ea048cddd3524f08ed29e88c6c3e3eda8
LAPTOP-N8AU3D6B\41369 <413694687@qq.com>
2019-09-10 20:31:09 +0800

    店铺详情ui

--- app/src/main/java/com/langt/zjgx/shop/ShopDetailActivity.java
+++ app/src/main/java/com/langt/zjgx/shop/ShopDetailActivity.java
@@ -1,5 +1,7 @@
 package com.langt.zjgx.shop;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -7,12 +9,19 @@ import com.langt.zjgx.R;
 import com.langt.zjgx.base.BaseActivity;
 import com.langt.zjgx.base.BasePresenter;
 import com.langt.zjgx.home.model.Banner;
+import com.langt.zjgx.home.model.GoodsBean;
+import com.langt.zjgx.shop.adapter.ShopDetailGoodsListAdapter;
 import com.langt.zjgx.widget.banner.BannerAdapter;
 import com.langt.zjgx.widget.banner.BannerLayout;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
+import q.rorbin.verticaltablayout.VerticalTabLayout;
+import q.rorbin.verticaltablayout.adapter.TabAdapter;
+import q.rorbin.verticaltablayout.widget.ITabView;
+import q.rorbin.verticaltablayout.widget.TabView;
 
 /**
  * 店铺详情
@@ -23,7 +32,17 @@ public class ShopDetailActivity extends BaseActivity {
     @BindView(R.id.layout_banner)
     BannerLayout bannerLayout;
 
+    @BindView(R.id.tablayout)
+    VerticalTabLayout tabLayout;
+    @BindView(R.id.rv_goods_list)
+    RecyclerView rv_goods_list;
+
     private BannerAdapter<Banner> mBannerAdapter;
+
+    private List<GoodsBean> goodsBeanList;
+    private ShopDetailGoodsListAdapter goodsListAdapter;
+
+    private String[] tabs = {"休闲零食","蔬果生鲜","百  货","母婴精品","童装鞋帽","居家百货"};
 
     @Override
     protected BasePresenter createPresenter() {
@@ -55,5 +74,64 @@ public class ShopDetailActivity extends BaseActivity {
         bannerList.add(new Banner());
         bannerList.add(new Banner());
         mBannerAdapter.reset(bannerList);
+
+        createTabs();
+
+        initGoodsListView();
+    }
+
+    private void createTabs() {
+        tabLayout.setTabAdapter(new TabAdapter() {
+            @Override
+            public int getCount() {
+                return tabs.length;
+            }
+
+            @Override
+            public ITabView.TabBadge getBadge(int position) {
+                return null;
+            }
+
+            @Override
+            public ITabView.TabIcon getIcon(int position) {
+                return null;
+            }
+
+            @Override
+            public ITabView.TabTitle getTitle(int position) {
+                return new ITabView.TabTitle.Builder().setContent(tabs[position]).build();
+            }
+
+            @Override
+            public int getBackground(int position) {
+                return 0;
+            }
+        });
+
+        tabLayout.addOnTabSelectedListener(new VerticalTabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabView tab, int position) {
+                tab.getTitleView().setTextColor(getResources().getColor(R.color.colortheme));
+                tab.getTitleView().setBackgroundColor(getResources().getColor(R.color.white));
+            }
+
+            @Override
+            public void onTabReselected(TabView tab, int position) {
+
+            }
+        });
+
+    }
+
+    private void initGoodsListView(){
+        rv_goods_list.setLayoutManager(new LinearLayoutManager(this));
+        goodsBeanList = new ArrayList<>();
+        goodsListAdapter = new ShopDetailGoodsListAdapter(goodsBeanList);
+        rv_goods_list.setAdapter(goodsListAdapter);
+
+        for (int i = 0; i < 10; i++) {
+            goodsBeanList.add(new GoodsBean(""));
+        }
+        goodsListAdapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/com/langt/zjgx/shop/adapter/ShopDetailGoodsListAdapter.java
@@ -0,0 +1,36 @@
+package com.langt.zjgx.shop.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.widget.TextView;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.langt.zjgx.R;
+import com.langt.zjgx.home.model.GoodsBean;
+
+import org.raphets.roundimageview.RoundImageView;
+
+import java.util.List;
+
+/**
+ * 店铺详情页面商品列表adapter
+ */
+public class ShopDetailGoodsListAdapter extends BaseQuickAdapter<GoodsBean, BaseViewHolder> {
+    public ShopDetailGoodsListAdapter(@Nullable List<GoodsBean> data) {
+        super(R.layout.item_shop_detail_goods_list, data);
+    }
+
+    @Override
+    protected void convert(@NonNull BaseViewHolder helper, GoodsBean item) {
+        RoundImageView iv_goods_pic = helper.getView(R.id.iv_goods_pic);
+        TextView tv_goods_name = helper.getView(R.id.tv_goods_name);
+        TextView tv_goods_price = helper.getView(R.id.tv_goods_price);
+        TextView tv_shop_seal_num = helper.getView(R.id.tv_shop_seal_num);
+        TextView tv_choose_specification = helper.getView(R.id.tv_choose_specification);
+
+        tv_goods_name.setText("03绿树果南非西柚新鲜红心葡萄柚孕妇水果应季蜜柚产地.");
+        tv_goods_price.setText(mContext.getString(R.string.goods_price,"125.00"));
+        tv_shop_seal_num.setText(mContext.getString(R.string.shop_goods_seal_count,"125.00"));
+    }
+}
